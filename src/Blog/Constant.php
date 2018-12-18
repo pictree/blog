cf2faa2c1fc76d58ee322ea25ade86ec7881d0c6
zhengjunyuan <zhengjunyuan@peilian.com>
2018-12-18 17:07:29 +0800

    edit post

---
+++ src/Blog/Constant.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZhengJY
+ * Date: 2018/12/18
+ * Time: 16:45
+ */
+namespace Pictree\Blog;
+
+class Constant
+{
+    const NAME = 'Constant';
+
+    public function className()
+    {
+        echo self::NAME, PHP_EOL;
+//        return self::NAME;
+    }
+}
